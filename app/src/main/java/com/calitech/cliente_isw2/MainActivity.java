e179c3eb966f55344d2495118fa4219b8c986231
Manacho <ma.ignacioperezc@gmail.com>
2017-05-24 18:31:30 -0400

    Union de Angelo Manuel funcional

--- app/src/main/java/com/calitech/cliente_isw2/MainActivity.java
+++ app/src/main/java/com/calitech/cliente_isw2/MainActivity.java
@@ -36,34 +36,6 @@ public class MainActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_main);
 
-        //************
-/*
-        lv1 =(ListView)findViewById(R.id.list1);
-        ArrayAdapter <String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, paises);
-/*
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-/*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Agregar lugar a itinerario", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-*/
-        //***********
 
         edtClave = (EditText) findViewById(R.id.edtLoginClave);
         edtCorreo = (EditText) findViewById(R.id.edtLoginCorreo);
@@ -107,47 +79,19 @@ public class MainActivity extends AppCompatActivity
         startActivity(new Intent(this,Registro.class));
     }
     public void Omitir(View v){
-        //startActivity(new Intent(this,resultado_lugares.class));
-        setContentView(R.layout.content_main);
-        setContentView(R.layout.app_bar_main);
+        startActivity(new Intent(this,Home.class));
+        setContentView(R.layout.content_home);
+        setContentView(R.layout.app_bar_home);
     }
 
-    /*
-        @Override
-        public void onBackPressed() {
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            if (drawer.isDrawerOpen(GravityCompat.START)) {
-                drawer.closeDrawer(GravityCompat.START);
-            } else {
-                super.onBackPressed();
-            }
-        }
-    */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    /*
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
 
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-
-            return super.onOptionsItemSelected(item);
-        }
-
-        @SuppressWarnings("StatementWithEmptyBody")
-
-    */
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
