6ae8f0b0dc854e64bc0ab78ecb3be29c9de1c22f
manuel <ma.ignacioperezc@gmail.com>
2017-05-25 17:02:39 -0400

    Drawers en todas las vistas

--- app/src/main/java/com/calitech/cliente_isw2/Home.java
+++ app/src/main/java/com/calitech/cliente_isw2/Home.java
@@ -41,7 +41,7 @@ public class Home extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Crear Itinerario", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
